45581f8b566e86930329382bfc6bce02c3ccaf71
wyliadrian <weiyi.li@yale.edu>
2022-07-22 22:58:28 -0400

    update

--- dev/get_raw_data.sh
+++ dev/get_raw_data.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 cd /Users/weiyili/Desktop/Projects/user-behavior-analysis/
-# mkdir data
-# mkdir data/raw
+#mkdir data
+#mkdir data/raw
 
-# Run script to download raw files from AWS S3 bucket
+#Run script to download raw files from AWS S3 bucket
+#chmod +x dev/raw_data.py
 ./dev/raw_data.py
 
-# Unzip files and move to data folder
+#Unzip files and move to data folder
 unzip data.zip
 rm data.zip
 mv data/*.csv data/raw
