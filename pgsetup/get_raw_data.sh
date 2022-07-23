e82a19204c67b72e7b110cfc58bd305f239c68cd
wyliadrian <weiyi.li@yale.edu>
2022-07-23 16:01:02 -0400

    update

--- pgsetup/get_raw_data.sh
+++ pgsetup/get_raw_data.sh
@@ -5,7 +5,7 @@ cd /Users/weiyili/Desktop/Projects/user-behavior-analysis/
 
 #Run script to download raw files from AWS S3 bucket
 #chmod +x dev/raw_data.py
-./dev/raw_data.py
+./pgsetup/raw_data.py
 
 #Unzip files and move to data folder
 unzip data.zip
